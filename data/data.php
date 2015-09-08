494a88586877206afea090ad8f2447c4cb0f995b
mdiallo2 <mousto87@hotmail.fr>
2015-09-08 16:44:22 -0400

    mis à jour du dossier

---
+++ data/data.php
@@ -0,0 +1,22 @@
+<?php
+define('P_PROD', 'produits');
+define('P_NAME', 'name');
+define('P_PRICE', 'price');
+define('P_DESCRIP', 'description');
+$menu = array(
+    'Accueil' => 'index.php', // <a href="index.php">Accueil</a>
+    'Contact' => 'contact.php',
+    'catalogue' => 'catalogue.php',
+    'produit' => 'produit.php');
+
+function mafonction()
+
+    {
+       array(
+            '102' => array('name'=>'homme', 'prix' => 145,69),
+
+         /*   '136' => array('name'=>'', '' => ),
+            '025' => array('name'=>'', '' =>),*/
+       );
+    }
+
