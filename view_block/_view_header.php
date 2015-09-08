ca47bc5d510582f83eb6ff8f958976135a956cab
mdiallo2 <mousto87@hotmail.fr>
2015-09-08 16:43:11 -0400

    mis à jour du dossier

---
+++ view_block/_view_header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>africa style</title>
+</head>
+<body>
+<h2><?php echo $view_data['title']?></h2>
+
+<div id="menu">
+    <ul>
+        <?php
+        foreach ($menu as $liste => $nomf) {
+            echo '<li><a href="', $nomf ,'">', $liste ,'</a></li>';
+        }
+        ?>
+    </ul>
+</div>
